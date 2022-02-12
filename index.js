90686bdcdb35292479dc2d36b126865f95a97bd9
vvmakarov <vvmakarov@corp.finam.ru>
2022-02-12 12:12:47 +0300

    2022-02-12--3

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ class Estimate {
     } else {
       leftString = '';
     }
-    const processedTime = options.showTakenTime ? `${reset} / Прошло ${magenta}${this.getTaken()} ${reset}` : '';
+    const processedTime = options.showTakenTime ? `${reset} / Left ${magenta}${this.getTaken()} ${reset}` : '';
     const roll = options.roll || 0;
     const msgAdd = options.msg || '';
     const msg = `${reset}Processed ${processedString}${reset}. ${leftString}${processedTime}${msgAdd ? `${reset} / ${msgAdd}` : ''} `;
